8d3cdce86fa817e08911d05990884b6854b70951
serg84bond2007 <32565458+serg84bond2007@users.noreply.github.com>
2018-11-27 11:38:49 +0200

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -17,17 +17,3 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-}
-@Bean
-public SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
-    SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
-    springBootMetricsCollector.register();
-    return springBootMetricsCollector;
-}
-
-@Bean
-public ServletRegistrationBean servletRegistrationBean() {
-    DefaultExports.initialize();
-    return new ServletRegistrationBean(new MetricsServlet(), "/prometheus");
-}
